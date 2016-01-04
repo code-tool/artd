63516acba8dcbed7f4e600cf1c577edcbe2cc227
Yuriy Berest <djua.com@gmail.com>
2016-01-04 11:27:39 +0200

    Fix default behaviour

--- bin/artifact-downloader.php
+++ bin/artifact-downloader.php
@@ -7,7 +7,7 @@ $container = require_once __DIR__ . '/../src/container.php';
 $artifactDownloader = $container['artifact_downloader'];
 
 try {
-    $returnCode = $artifactDownloader->work();
+    $returnCode = $artifactDownloader->work(true);
 } catch (Exception $e) {
     $returnCode = 1;
     echo $e->getMessage(), PHP_EOL, $e->getTraceAsString();

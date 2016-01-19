ca7ecc50f1226ec0e45267c53b4d47834b32dc33
Yuriy Berest <djua.com@gmail.com>
2016-01-19 15:06:38 +0200

    Workaround for old curl

--- bin/artifact-downloader.php
+++ bin/artifact-downloader.php
@@ -3,6 +3,19 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 $container = require_once __DIR__ . '/../src/container.php';
 
+// Fix warnings on old curl
+if (false === defined('CURLOPT_TCP_KEEPALIVE')) {
+    define('CURLOPT_TCP_KEEPALIVE', 213);
+}
+
+if (false === defined('CURLOPT_TCP_KEEPIDLE')) {
+    define('CURLOPT_TCP_KEEPIDLE', 214);
+}
+
+if (false === defined('CURLOPT_TCP_KEEPINTVL')) {
+    define('CURLOPT_TCP_KEEPINTVL', 215);
+}
+
 /** @var \CodeTool\ArtifactDownloader\UnitConfig\UnitConfigInterface $unitConfig */
 $unitConfig = $container['unit_config'];
 /** @var \CodeTool\ArtifactDownloader\ArtifactDownloader $artifactDownloader */

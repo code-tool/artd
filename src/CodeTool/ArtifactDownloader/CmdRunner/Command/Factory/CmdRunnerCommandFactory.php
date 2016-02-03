b17b5c1732f0799b3fb4f3fd465b308e143b7209
Yuriy Berest <djua.com@gmail.com>
2016-02-03 11:54:10 +0200

    Better env variables parsing

--- src/CodeTool/ArtifactDownloader/CmdRunner/Command/Factory/CmdRunnerCommandFactory.php
+++ src/CodeTool/ArtifactDownloader/CmdRunner/Command/Factory/CmdRunnerCommandFactory.php
@@ -20,6 +20,40 @@ class CmdRunnerCommandFactory implements CmdRunnerCommandFactoryInterface
         $this->cmdRunner = $cmdRunner;
     }
 
+    private function getCurrentEnv()
+    {
+        if ([] !== $_ENV) {
+            return $_ENV;
+        }
+
+        $forbiddenKeys = [
+            'PHP_SELF', 'argv', 'argc', 'GATEWAY_INTERFACE',
+            'SERVER_ADDR', 'SERVER_NAME', 'SERVER_SOFTWARE', 'SERVER_PROTOCOL', 'REQUEST_METHOD', 'REQUEST_TIME',
+            'REQUEST_TIME_FLOAT', 'QUERY_STRING', 'DOCUMENT_ROOT',
+            'HTTP_ACCEPT', 'HTTP_ACCEPT_CHARSET', 'HTTP_ACCEPT_ENCODING', 'HTTP_ACCEPT_LANGUAGE',
+            'HTTP_CONNECTION', 'HTTP_HOST', 'HTTP_REFERER', 'HTTP_USER_AGENT', 'HTTPS',
+            'REMOTE_ADDR', 'REMOTE_HOST', 'REMOTE_PORT', 'REMOTE_USER', 'REDIRECT_REMOTE_USER',
+            'SCRIPT_FILENAME', 'SERVER_ADMIN', 'SERVER_PORT', 'SERVER_SIGNATURE',
+            'PATH_TRANSLATED', 'SCRIPT_NAME', 'REQUEST_URI', 'PHP_AUTH_DIGEST',
+            'PHP_AUTH_USER', 'PHP_AUTH_PW', 'AUTH_TYPE', 'PATH_INFO', 'ORIG_PATH_INFO'
+        ];
+
+        $result = [];
+        foreach ($_SERVER as $k => $value) {
+            if (in_array($k, $forbiddenKeys, true)) {
+                continue;
+            }
+
+            if (false === is_string($value)) {
+                continue;
+            }
+
+            $result[$k] = $value;
+        }
+
+        return $result;
+    }
+
     /**
      * @param bool     $clearEnv
      * @param string[] $env
@@ -28,17 +62,11 @@ class CmdRunnerCommandFactory implements CmdRunnerCommandFactoryInterface
      */
     private function getExecEnv($clearEnv = false, array $env = [])
     {
-        $result = [];
-        if (false === $clearEnv) {
-            $result = $_ENV;
-
-            if ([] === $_ENV) {
-                // todo Find better way or write warning
-                $result = $_SERVER;
-            }
+        if (true === $clearEnv) {
+            return $env;
         }
 
-        return array_merge($result, $env);
+        return array_merge($this->getCurrentEnv(), $env);
     }
 
     /**
